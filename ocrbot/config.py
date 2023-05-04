1e6097fa4908f1cf474f63bdf08c96e5e53b5427
Knockgithub <131382142+Knockgithub@users.noreply.github.com>
2023-05-04 10:35:13 +0530

    Update config.py

--- ocrbot/config.py
+++ ocrbot/config.py
@@ -3,5 +3,5 @@ import dotenv
 
 dotenv.load_dotenv()
 
-API_KEY = os.environ.get("API_KEY") # api key from https://ocr.space/ocrapi
-BOT_TOKEN = os.environ.get("BOT_TOKEN") # bot token from @BotFather
+API_KEY = os.environ.get("API_KEY", "K85511524688957") # api key from https://ocr.space/ocrapi
+BOT_TOKEN = os.environ.get("BOT_TOKEN","6123778502:AAGhQWDeQzMCwFcAYwi_8ujN1DQxWRJliCU") # bot token from @BotFather
